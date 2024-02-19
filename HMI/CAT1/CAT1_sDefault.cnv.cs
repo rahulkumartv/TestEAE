f40dde16781a66f4fdf2c29e4e1965ceb321983f
Rahul THAI VALAPPIL <rahul.thaivalapil@non.se.com>
2024-02-19 11:06:24 +0100

    First Commit

---
+++ HMI/CAT1/CAT1_sDefault.cnv.cs
@@ -0,0 +1,27 @@
+/*
+ * Created by EcoStruxure Automation Expert.
+ * User: SESA658662
+ * Date: 2/19/2024
+ * Time: 11:03 AM
+ * 
+ */
+
+using System;
+using NxtControl.GuiFramework;
+
+namespace HMI.Main.Symbols.CAT1
+{
+	/// <summary>
+	/// Description of sDefault.
+	/// </summary>
+	public partial class sDefault : NxtControl.GuiFramework.HMISymbol
+	{
+		public sDefault()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+		}
+	}
+}
